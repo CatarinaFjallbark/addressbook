f9b64810a775de94239beafa35dbf7e5ac05d441
Catarina Sörensen <catarina.sorensen@jayway.com>
2021-11-21 11:52:37 +0100

    Basic set up and fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState, ReactElement } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  React.useEffect(() => {
-    console.log("mount");
-  });
+function App(): ReactElement {
+  type Results = Record<string, any>[];
+  type Response = {
+    info: Record<string, any>;
+    results: Results;
+  };
+  type State = Results | undefined;
 
+  const [data, dataSet] = useState<State>(undefined);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://randomuser.me/api/?results=10");
+      const json = (await response.json()) as Response;
+      dataSet(json.results);
+      console.log("data", json.results);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
